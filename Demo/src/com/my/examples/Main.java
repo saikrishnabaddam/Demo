1d794d0e66c85bff93a38303256a056d829de6c1
saikrishnabaddam <saikrishnagoud.baddam@gmail.com>
2015-10-13 22:05:26 +0530

    Initial checkin of Demo Project

---
+++ Demo/src/com/my/examples/Main.java
@@ -0,0 +1,15 @@
+package com.my.examples;
+
+public class Main {
+public static void main(String[] args){
+	
+	Sub sub=new Sub();
+	
+	sub.display();
+	displayGreeting();
+}
+
+public static void displayGreeting(){
+	System.out.println("Welcome to the Demo Project");
+}
+}
---
+++ Demo/src/com/my/examples/Sub.java
@@ -0,0 +1,7 @@
+package com.my.examples;
+
+public class Sub {
+public void display(){
+	System.out.println("Welcome Sub class display method");
+}
+}
