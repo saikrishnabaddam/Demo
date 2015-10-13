c0672773803b8b5899eb2df5a57c82825261c1f4
saikrishnabaddam <saikrishnagoud.baddam@gmail.com>
2015-10-13 23:05:01 +0530

    Added a simple output statement

--- Demo/src/com/my/examples/Main.java
+++ Demo/src/com/my/examples/Main.java
@@ -12,5 +12,7 @@ public static void main(String[] args){
 public static void displayGreeting(){
 	System.out.println("Welcome to the Demo Project");
 	System.out.println("This is test page");
+	
+	System.out.println("Test 1");
 }
 }
