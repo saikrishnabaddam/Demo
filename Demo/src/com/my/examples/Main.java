9fbf295f93c415c11ad2f864d24ccc3568aa9ab8
saikrishnabaddam <saikrishnagoud.baddam@gmail.com>
2015-10-13 22:17:37 +0530

    My first commit

--- Demo/src/com/my/examples/Main.java
+++ Demo/src/com/my/examples/Main.java
@@ -11,5 +11,6 @@ public static void main(String[] args){
 
 public static void displayGreeting(){
 	System.out.println("Welcome to the Demo Project");
+	System.out.println("This is test page");
 }
 }
